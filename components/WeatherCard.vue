74ffdac8874544ae7e19ffda7540d0b2e3e8445c
Johnny Xhama <xxhama@me.com>
2019-08-07 17:36:10 -0500

    trimmed template, added darkSki Test Api data

--- components/WeatherCard.vue
+++ components/WeatherCard.vue
@@ -66,25 +66,27 @@
   </v-card>
 </template>
 <script>
+const testData = [
+  {
+    day: 'Tuesday',
+    icon: 'mdi-white-balance-sunny',
+    temp: '24\xB0/12\xB0'
+  },
+  {
+    day: 'Wednesday',
+    icon: 'mdi-white-balance-sunny',
+    temp: '22\xB0/14\xB0'
+  },
+  { day: 'Thursday', icon: 'mdi-cloud', temp: '25\xB0/15\xB0' }
+]
+
 export default {
   name: 'WeatherCard',
   data: () => ({
     loading: false,
     labels: ['SU', 'MO', 'TU', 'WED', 'TH', 'FR', 'SA'],
     time: 0,
-    forecast: [
-      {
-        day: 'Tuesday',
-        icon: 'mdi-white-balance-sunny',
-        temp: '24\xB0/12\xB0'
-      },
-      {
-        day: 'Wednesday',
-        icon: 'mdi-white-balance-sunny',
-        temp: '22\xB0/14\xB0'
-      },
-      { day: 'Thursday', icon: 'mdi-cloud', temp: '25\xB0/15\xB0' }
-    ]
+    forecast: testData
   })
 }
 </script>
